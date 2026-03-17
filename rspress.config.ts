1869d9ef8323a04a14edf6651ecc1f6dadee1d7b
Viliam Krizan <vkrizan@redhat.com>
2026-03-17 13:25:52 +0100

    Add Edit this page option

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from '@rspress/core';
 export default defineConfig({
   root: 'docs',
   base: '/foreman-iop-handbook/',
+  themeConfig: {
+    editLink: {
+      docRepoBaseUrl: 'https://github.com/vkrizan/foreman-iop-handbook/tree/main/docs',
+    },
+  },
 });
 
